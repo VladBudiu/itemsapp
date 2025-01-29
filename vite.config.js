3802b9f565b231b6baeaa1423835c7812a7e7032
VladBudiu <vlad.adrian75@gmail.com>
2025-01-29 04:03:07 +0200

    deployment fix 2

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://umy34nu3mj.execute-api.eu-north-1.amazonaws.com/prod',
+        target: 'https://umy34nu3mj.execute-api.eu-north-1.amazonaws.com/production',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
